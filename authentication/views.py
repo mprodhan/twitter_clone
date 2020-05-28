3732158297ebbdfb5f23e449d79f586113c1ab3c
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2020-05-28 07:37:19 -0400

    will indeed merge once authentication is accomplished

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, HttpResponseRedirect, HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from authentication.models import UserProfile
 
 # Create your views here.
